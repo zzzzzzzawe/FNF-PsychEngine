d22ab438264fc8f9631325a7b69a8a015fb2740a
Lily <egzozu.be.bas@gmail.com>
2023-12-31 17:27:32 +0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -52,6 +52,10 @@ class Main extends Sprite
 	public static function main():Void
 	{
 		Lib.current.addChild(new Main());
+		#if cpp
+                cpp.NativeGc.enable(true);
+                cpp.NativeGc.run(true);
+                #end
 	}
 
 	public function new()
@@ -114,6 +118,8 @@ class Main extends Sprite
 			game.width = Math.ceil(stageWidth / game.zoom);
 			game.height = Math.ceil(stageHeight / game.zoom);
 		}
+
+		#if (android && EXTERNAL || MEDIA) checkPermissions() #end
 	
 		#if LUA_ALLOWED llua.Lua.set_callbacks_function(cpp.Callable.fromStaticFunction(psychlua.CallbackHandler.call)); #end
 		Controls.instance = new Controls();
