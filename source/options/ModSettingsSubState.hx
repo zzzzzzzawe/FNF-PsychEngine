128ed08d61179d264b38ebeb30351dea28b30ebd
Lily <egzozu.be.bas@gmail.com>
2024-01-09 12:59:33 +0300

    Update ModSettingsSubState.hx

--- source/options/ModSettingsSubState.hx
+++ source/options/ModSettingsSubState.hx
@@ -129,7 +129,7 @@ class ModSettingsSubState extends BaseOptionsMenu
 		{
 			var errorTitle = 'Mod name: ' + folder;
 			var errorMsg = 'An error occurred: $e';
-			#if windows
+			#if (desktop || mobile)
 			lime.app.Application.current.window.alert(errorMsg, errorTitle);
 			#end
 			trace('$errorTitle - $errorMsg');
