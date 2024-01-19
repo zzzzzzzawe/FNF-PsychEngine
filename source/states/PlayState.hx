abfdbfd790ac5a5bfe135c878db8ab6547d22a3d
Lily <egzozu.be.bas@gmail.com>
2024-01-19 22:31:07 +0300

    just in case

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -1118,7 +1118,7 @@ class PlayState extends MusicBeatState
 				daNote.ignoreNote = true;
 
 				unspawnNotes.remove(daNote);
-				daNote.kill();
+				if(!ClientPrefs.data.lowQuality || !ClientPrefs.data.popUpRating) daNote.kill();
 				daNote.destroy();
 			}
 			--i;
@@ -3160,7 +3160,7 @@ class PlayState extends MusicBeatState
 
 	public function invalidateNote(note:Note):Void {
 		notes.remove(note, true);
-		note.kill();
+		if(!ClientPrefs.data.lowQuality || !ClientPrefs.data.popUpRating) note.kill();
 		note.destroy();
 	}
 
