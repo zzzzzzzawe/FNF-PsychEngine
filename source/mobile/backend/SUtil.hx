121b84e0df216b00aa3ffcbf6dbecf4c5c911c33
Lily <egzozu.be.bas@gmail.com>
2024-01-24 14:37:19 +0300

    just gtfo apple please

--- source/mobile/backend/SUtil.hx
+++ source/mobile/backend/SUtil.hx
@@ -54,7 +54,7 @@ class SUtil
 				daPath = Environment.getExternalStorageDirectory() + '/Android/media/' + Application.current.meta.get('packageName');
 		}
 		#elseif ios
-		daPath = haxe.io.Path.join([LimeSystem.documentsDirectory, Application.current.meta.get('file')]);
+		daPath = LimeSystem.documentsDirectory;
 		#end
 
 		return daPath;
