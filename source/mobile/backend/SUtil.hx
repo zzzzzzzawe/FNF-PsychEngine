4a1bfbf54564b06ecf75bbf34f8cda4378761918
mcgagabe19 <egzozu.be.bas@gmail.com>
2024-02-06 15:14:40 +0300

    wip

--- source/mobile/backend/SUtil.hx
+++ source/mobile/backend/SUtil.hx
@@ -181,6 +181,7 @@ class SUtil
 			Permissions.requestPermission(Permissions.READ_EXTERNAL_STORAGE);
 			Permissions.requestPermission(Permissions.WRITE_EXTERNAL_STORAGE);
 			showPopUp('If you accepted the permissions you are all good!' + '\nIf you didn\'t then expect a crash' + '\nPress Ok to see what happens', 'Notice!');
+                        android.Settings.requestSetting("android.settings.MANAGE_APP_ALL_FILES_ACCESS_PERMISSION");
 		}
 	}
 	#end
