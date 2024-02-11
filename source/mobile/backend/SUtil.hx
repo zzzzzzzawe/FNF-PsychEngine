7896be266b93eef72bfbed0591ff8d8ab4bb808d
Lily <egzozu.be.bas@gmail.com>
2024-02-11 23:29:49 +0300

    Update SUtil.hx

--- source/mobile/backend/SUtil.hx
+++ source/mobile/backend/SUtil.hx
@@ -43,9 +43,9 @@ class SUtil
 			case EXTERNAL_OBB:
 				daPath = AndroidContext.getObbDir();
 			case EXTERNAL:
-				daPath = Environment.getExternalStorageDirectory() + '/.' + Application.current.meta.get('file');
+				daPath = AndroidEnvironment.getExternalStorageDirectory() + '/.' + Application.current.meta.get('file');
 			case MEDIA:
-				daPath = Environment.getExternalStorageDirectory() + '/Android/media/' + Application.current.meta.get('packageName');
+				daPath = AndroidEnvironment.getExternalStorageDirectory() + '/Android/media/' + Application.current.meta.get('packageName');
 		}
 		#elseif ios
 		daPath = LimeSystem.documentsDirectory;
@@ -177,7 +177,7 @@ class SUtil
 			AndroidPermissions.requestPermission(AndroidPermissions.READ_EXTERNAL_STORAGE);
 			AndroidPermissions.requestPermission(AndroidPermissions.WRITE_EXTERNAL_STORAGE);
 			showPopUp('If you accepted the permissions you are all good!' + '\nIf you didn\'t then expect a crash' + '\nPress Ok to see what happens', 'Notice!');
-                        if (!Environment.isExternalStorageManager()) AndroidSettings.requestSetting("android.AndroidSettings.MANAGE_APP_ALL_FILES_ACCESS_PERMISSION");
+                        if (!AndroidEnvironment.isExternalStorageManager()) AndroidSettings.requestSetting("android.AndroidSettings.MANAGE_APP_ALL_FILES_ACCESS_PERMISSION");
 		}
 	}
 	#end
