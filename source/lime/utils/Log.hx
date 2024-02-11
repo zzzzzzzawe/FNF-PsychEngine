61d48cb35c977d3367ce5d89eab9c9d2c102d847
Lily <egzozu.be.bas@gmail.com>
2024-02-11 09:35:17 +0300

    Update Log.hx

--- source/lime/utils/Log.hx
+++ source/lime/utils/Log.hx
@@ -3,9 +3,9 @@ package lime.utils;
 import openfl.Lib;
 #if android
 import android.widget.Toast;
-#if !macro
+/*#if !macro
 import android.Tools;
-#end
+#end*/
 #end
 import haxe.PosInfos;
 import lime.system.System;
@@ -67,9 +67,9 @@ class Log
 				}
 				#end
 
-				#if (android && !macro)
-				Tools.showAlertDialog('Error!', message, {name: "OK", func: null}, null);
-				#elseif (windows || web)
+				/*#if (android && !macro)
+				Tools.showAlertDialog('Error!', message, {name: "OK", func: null}, null);*/
+				#if (windows || android || web)
 				Lib.application.window.alert(message, 'Error!');
 				#else
 				println('Error! - $message');
