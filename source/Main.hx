42ad5d60a9e03b60033ccd60440f1b41331d740f
KarimAkra <karimakra512@gmail.com>
2024-02-18 17:45:31 +0200

    SOWWY

--- source/Main.hx
+++ source/Main.hx
@@ -121,7 +121,7 @@ class Main extends Sprite
 
 		#if mobile
 		if(!CopyState.checkExistingFiles())
-			game.initialState = CopyState
+			game.initialState = CopyState;
 		#end
 
 		addChild(new FlxGame(#if (openfl >= "9.2.0") 1280, 720 #else game.width, game.height #end, game.initialState, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
