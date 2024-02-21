08bbe3188378e8aa32e7ae311f8a55757fa885db
KarimAkra <karimakra512@gmail.com>
2024-02-21 22:01:13 +0200

    hopefully fixed charting shit...

--- source/states/editors/ChartingState.hx
+++ source/states/editors/ChartingState.hx
@@ -1767,7 +1767,7 @@ class ChartingState extends MusicBeatState
 					&& touch.x < gridBG.x + gridBG.width
 					&& touch.y > gridBG.y
 					&& touch.y < gridBG.y + (GRID_SIZE * getSectionBeats() * 4) * zoomList[curZoom]
-					&& touch.justPressed)
+					&& touch.justReleased)
 				{
 					FlxG.log.add('added note');
 					addNote();
