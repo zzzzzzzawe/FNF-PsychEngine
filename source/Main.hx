c3eeed3938209ea62a5adc8330c79004b5b3c163
mcgagabe19 <egzozu.be.bas@gmail.com>
2024-02-22 19:10:43 +0300

    I'm tired

--- source/Main.hx
+++ source/Main.hx
@@ -60,12 +60,12 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
+                #if (android && EXTERNAL || MEDIA)
+                SUtil.doPermissionsShit();
+                #end
                 #if mobile
                 Sys.setCwd(#if (android)Path.addTrailingSlash(#end SUtil.getStorageDirectory()#if (android))#end);
                 #end
-		#if (android && EXTERNAL || MEDIA)
-		SUtil.doPermissionsShit();
-		#end
 		mobile.backend.CrashHandler.init();
 
 		#if windows
