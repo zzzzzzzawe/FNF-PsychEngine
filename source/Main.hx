164a27b94d619b72a5f84d732f5e1c049e696ee9
KarimAkra <karimakra512@gmail.com>
2024-02-27 22:18:32 +0200

    tf was this

--- source/Main.hx
+++ source/Main.hx
@@ -1,22 +1,17 @@
 package;
 
 import debug.FPSCounter;
-
-import flixel.graphics.FlxGraphic;
 import flixel.FlxGame;
-import flixel.FlxState;
 import haxe.io.Path;
-import openfl.Assets;
-import openfl.system.System;
 import openfl.Lib;
 import openfl.display.Sprite;
 import openfl.events.Event;
 import openfl.display.StageScaleMode;
 import lime.system.System as LimeSystem;
-import lime.app.Application;
 import states.TitleState;
 import openfl.events.KeyboardEvent;
 import mobile.states.CopyState;
+import mobile.objects.MobileControls;
 #if linux
 import lime.graphics.Image;
 
@@ -39,12 +34,8 @@ class Main extends Sprite
 
 	public static var fpsVar:FPSCounter;
 
-	#if mobile
-	public static final platform:String = "Phones";
-	#else
-	public static final platform:String = "PCs";
-	#end
-
+	public static final platform:String = #if mobile "Phones" #else "PCs" #end;
+	
 	// You can pretty much ignore everything from here on - your code should go in your states.
 
 	public static function main():Void
@@ -77,22 +68,14 @@ class Main extends Sprite
 		")
 		#end
 
-		if (stage != null)
-		{
-			init();
-		}
-		else
-		{
-			addEventListener(Event.ADDED_TO_STAGE, init);
-		}
+		if (stage != null) init();
+		else addEventListener(Event.ADDED_TO_STAGE, init);
 	}
 
 	private function init(?E:Event):Void
 	{
 		if (hasEventListener(Event.ADDED_TO_STAGE))
-		{
 			removeEventListener(Event.ADDED_TO_STAGE, init);
-		}
 
 		setupGame();
 	}
@@ -110,54 +93,45 @@ class Main extends Sprite
 			game.width = Math.ceil(stageWidth / game.zoom);
 			game.height = Math.ceil(stageHeight / game.zoom);
 		}
-	
+		
+		addChild(new FlxGame(#if (openfl >= "9.2.0") 1280, 720 #else game.width, game.height #end, #if mobile CopyState.checkExistingFiles() ? game.initialState : CopyState #else game.initialState #end, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
+		fpsVar = new FPSCounter(10, 3, 0xFFFFFF);
+		addChild(fpsVar);
+		if(fpsVar != null)
+			fpsVar.visible = ClientPrefs.data.showFPS;
+		Lib.current.stage.align = "tl";
+		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
+		FlxG.game.stage.quality = openfl.display.StageQuality.LOW;
+
+		#if linux
+		var icon = Image.fromFile("icon.png");
+		Lib.current.stage.window.setIcon(icon);
+		#end
+		
+		#if ACHIEVEMENTS_ALLOWED Achievements.load(); #end
+		#if DISCORD_ALLOWED DiscordClient.prepare(); #end
 		#if LUA_ALLOWED llua.Lua.set_callbacks_function(cpp.Callable.fromStaticFunction(psychlua.CallbackHandler.call)); #end
+
 		Controls.instance = new Controls();
 		ClientPrefs.loadDefaultKeys();
 
-		#if ACHIEVEMENTS_ALLOWED Achievements.load(); #end
-		
-		addChild(new FlxGame(#if (openfl >= "9.2.0") 1280, 720 #else game.width, game.height #end, #if mobile CopyState.checkExistingFiles() ? game.initialState : CopyState #else game.initialState #end, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
-		
-		Achievements.load();
-		#if android
-		FlxG.android.preventDefaultKeys = [BACK];
-		#end
-		
-		fpsVar = new FPSCounter(10, 3, 0xFFFFFF);
-		addChild(fpsVar);
-		Lib.current.stage.align = "tl";
-		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
-                FlxG.game.stage.quality = openfl.display.StageQuality.LOW;
-		if(fpsVar != null)
-			fpsVar.visible = ClientPrefs.data.showFPS;
+		MobileControls.initSave();
+
+		#if desktop FlxG.stage.addEventListener(KeyboardEvent.KEY_UP, toggleFullScreen); #end
 
 		#if linux
 		var icon = Image.fromFile("icon.png");
 		Lib.current.stage.window.setIcon(icon);
 		#end
 
-		#if desktop
-		FlxG.stage.addEventListener(KeyboardEvent.KEY_UP, toggleFullScreen);
-		#end
+		#if android FlxG.android.preventDefaultKeys = [BACK]; #end
 
-		#if html5
-		FlxG.autoPause = false;
-		FlxG.mouse.visible = false;
-		#end
-
-		#if DISCORD_ALLOWED
-		DiscordClient.prepare();
-		#end
-
-		#if mobile
-		LimeSystem.allowScreenTimeout = ClientPrefs.data.screensaver;
-		#end
+		#if mobile LimeSystem.allowScreenTimeout = ClientPrefs.data.screensaver; #end
 
 		// shader coords fix
 		FlxG.signals.gameResized.add(function (w, h) {
 			if(fpsVar != null)
-				fpsVar.positionFPS(10, 3, Math.min(Lib.current.stage.stageWidth / FlxG.width, Lib.current.stage.stageHeight / FlxG.height));
+				fpsVar.positionFPS(10, 3, Math.min(w / FlxG.width, h / FlxG.height));
 		     if (FlxG.cameras != null) {
 			   for (cam in FlxG.cameras.list) {
 				if (cam != null && cam.filters != null)
@@ -168,20 +142,16 @@ class Main extends Sprite
 			if (FlxG.game != null)
 			resetSpriteCache(FlxG.game);
 		});
-
-		#if debug
-		FlxG.console.registerClass(openfl.utils.Assets);
-		#end
 	}
 
-	static function resetSpriteCache(sprite:Sprite):Void {
+	static function resetSpriteCache(sprite:Sprite) {
 		@:privateAccess {
-		        sprite.__cacheBitmap = null;
+		    sprite.__cacheBitmap = null;
 			sprite.__cacheBitmapData = null;
 		}
 	}
 
-	function toggleFullScreen(event:KeyboardEvent):Void {
+	function toggleFullScreen(event:KeyboardEvent) {
 		if(Controls.instance.justReleased('fullscreen'))
 			FlxG.fullscreen = !FlxG.fullscreen;
 	}
