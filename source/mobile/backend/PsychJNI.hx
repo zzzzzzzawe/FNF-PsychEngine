aa5b2be68cc431f5f7e20a8c75a211280245d7b3
Lily <egzozu.be.bas@gmail.com>
2024-03-07 19:42:28 +0300

    Update PsychJNI.hx

--- source/mobile/backend/PsychJNI.hx
+++ source/mobile/backend/PsychJNI.hx
@@ -1,5 +1,5 @@
 /*
-The functions needs to be added: getCurrentOrientation, setWindowStyle
+The functions needs to be added: setWindowStyle
 The functions "maybe" needs to be added: isScreenKeyboardShown, getDisplayDPI, messageboxShowMessageBox, clipboardHasText, clipboardGetText, clipboardSetText
 NOTE: THESE AT "SDLActivity"!!
 */
@@ -10,9 +10,15 @@ import lime.system.JNI;
 
 class PsychJNI #if (lime >= "8.0.0") implements JNISafety #end
 {
+	@:noCompletion private static var getCurrentOrientation_jni:Dynamic = JNI.createStaticMethod('org/libsdl/app/SDLActivity', 'getCurrentOrientation','()I');
 	@:noCompletion private static var setOrientation_jni:Dynamic = JNI.createStaticMethod('org/libsdl/app/SDLActivity', 'setOrientation','(IIZLjava/lang/String;)V');
 
 	public static inline function setOrientation(width:Int, height:Int, resizeable:Bool, hint:String):Dynamic
 		return setOrientation_jni(width, height, resizeable, hint);
+
+	public static inline function getCurrentOrientation():Dynamic
+		return getCurrentOrientation_jni();
+
+	// public static inline function getCurrentOrientationAsString():Dynamic {}
 }
 #end
