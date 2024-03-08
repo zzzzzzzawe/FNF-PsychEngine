f137ec6a807f40b3626c5a94d7c2eb8b1739e44c
Lily <egzozu.be.bas@gmail.com>
2024-03-08 09:37:18 +0300

    Update PsychJNI.hx

--- source/mobile/backend/PsychJNI.hx
+++ source/mobile/backend/PsychJNI.hx
@@ -1,6 +1,6 @@
 /*
-	The functions needs to be added: setWindowStyle
-	The functions "maybe" needs to be added: isScreenKeyboardShown, getDisplayDPI, messageboxShowMessageBox, clipboardHasText, clipboardGetText, clipboardSetText
+	The functions needs to be added: setWindowStyle, getCurrentOrientation
+	The functions "maybe" needs to be added: isScreenKeyboardShown, messageboxShowMessageBox, clipboardHasText, clipboardGetText, clipboardSetText
 	NOTE: THESE AT "SDLActivity"!!
  */
 
@@ -11,15 +11,9 @@ import lime.system.JNI;
 
 class PsychJNI #if (lime >= "8.0.0") implements JNISafety #end
 {
-	@:noCompletion private static var getCurrentOrientation_jni:Dynamic = JNI.createStaticMethod('org/libsdl/app/SDLActivity', 'getCurrentOrientation', '()I');
 	@:noCompletion private static var setOrientation_jni:Dynamic = JNI.createStaticMethod('org/libsdl/app/SDLActivity', 'setOrientation', '(IIZLjava/lang/String;)V');
-       @:noCompletion private static var getDisplayDPI_jni:Dynamic = JNI.createStaticMethod('org/libsdl/app/SDLActivity', 'getDisplayDPI', '()Landroid/util/DisplayMetrics;');
 
 	public static inline function setOrientation(width:Int, height:Int, resizeable:Bool, hint:String):Dynamic
 		return setOrientation_jni(width, height, resizeable, hint);
-
-	public static inline function getCurrentOrientation():Dynamic
-		return getCurrentOrientation_jni();
-        public static inline function getDisplayDPI():Dynamic return getDisplayDPI_jni();
 }
 #end
