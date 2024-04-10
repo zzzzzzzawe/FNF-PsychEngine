486e7404761132c86f75354275924c9a108b3594
Lily <egzozu.be.bas@gmail.com>
2024-04-11 00:56:22 +0300

    idrk

--- source/Main.hx
+++ source/Main.hx
@@ -55,7 +55,7 @@ class Main extends Sprite
 		#if (android && EXTERNAL || MEDIA)
 		SUtil.doPermissionsShit();
 		#end
-		Sys.setCwd(SUtil.getStorageDirectory(false));
+		Sys.setCwd(SUtil.getStorageDirectory());
 		#end
 		mobile.backend.CrashHandler.init();
 
