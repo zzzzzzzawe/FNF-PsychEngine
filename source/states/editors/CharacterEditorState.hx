8f45842471371e1d3ac644b0a780497f498f7e67
Lily <egzozu.be.bas@gmail.com>
2024-05-01 14:30:03 +0300

    stealing a pr

--- source/states/editors/CharacterEditorState.hx
+++ source/states/editors/CharacterEditorState.hx
@@ -4,7 +4,6 @@ import flixel.FlxObject;
 import flixel.graphics.FlxGraphic;
 
 import flixel.animation.FlxAnimation;
-import flixel.system.debug.interaction.tools.Pointer.GraphicCursorCross;
 import flixel.addons.ui.*;
 import flixel.ui.FlxButton;
 import flixel.util.FlxDestroyUtil;
@@ -19,6 +18,15 @@ import objects.Character;
 import objects.HealthIcon;
 import objects.Bar;
 import mobile.flixel.FlxButton as MobileButton;
+
+// flixel 5.7.0+ fix
+#if (FLX_DEBUG || flixel < version("5.7.0"))
+typedef PointerGraphic = flixel.system.debug.interaction.tools.Pointer.GraphicCursorCross;
+#else
+@:bitmap("assets/images/debugger/cursorCross.png")
+class PointerGraphic extends openfl.display.BitmapData {}
+#end
+
 class CharacterEditorState extends MusicBeatState
 {
 	var character:Character;
@@ -103,7 +111,7 @@ class CharacterEditorState extends MusicBeatState
 
 		addCharacter();
 
-		cameraFollowPointer = new FlxSprite().loadGraphic(FlxGraphic.fromClass(GraphicCursorCross));
+		cameraFollowPointer = new FlxSprite(FlxGraphic.fromClass(PointerGraphic));
 		cameraFollowPointer.setGraphicSize(40, 40);
 		cameraFollowPointer.updateHitbox();
 		add(cameraFollowPointer);
