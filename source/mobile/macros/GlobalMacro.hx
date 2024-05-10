65be2c182dbb215c3c17e8a13b9a8605365fd205
Lily <egzozu.be.bas@gmail.com>
2024-05-10 23:56:34 +0300

    r u kiddin

--- source/mobile/macros/GlobalMacro.hx
+++ source/mobile/macros/GlobalMacro.hx
@@ -6,7 +6,7 @@ class GlobalMacro {
     public static function haxelibWarning():Void {
         #if hxluajit
         Log.warn('hxluajit detected, using hxluajit is still experimental!!');
-        #elseif android && (linc_luajit && hxvlc)
+        #elseif (android && (linc_luajit && hxvlc))
         Log.warn('linc_luajit with hxvlc detected, lua will be die. Consider using EXPERIMENTAL_HXLUAJIT or ADVANCED_VIDEO_FUNCTIONS instead.');
         #end
     }
