542b4d032af50b7a832afd89aae0879a9dc87a59
Lily <egzozu.be.bas@gmail.com>
2024-05-29 19:03:43 +0300

    AAAA KARIM WHY

--- source/mobile/backend/SUtil.hx
+++ source/mobile/backend/SUtil.hx
@@ -30,7 +30,7 @@ class SUtil
 		if (!FileSystem.exists(rootDir + 'storagetype.txt'))
 			File.saveContent(rootDir + 'storagetype.txt', ClientPrefs.data.storageType);
 		var curStorageType:String = File.getContent(rootDir + 'storagetype.txt');
-		if(curStorageType == "EXTERNAL_DATA" && selectedDir == null)
+		if(curStorageType == "CUSTOM" && selectedDir == null)
 		{
 			initSAFCallback();
 			Tools.openDirectoryPicker(5);
